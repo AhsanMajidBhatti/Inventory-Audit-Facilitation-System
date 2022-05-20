e2b32f795aee641d294a8ae243a77ff142c6093a
Ahsan Majid Bhatti <ahsanmajidbhatti49@gmail.com>
2022-05-20 11:53:10 +0500

    Updated

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,7 +14,7 @@ return [
     */
 
     // 'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1:8000,inventory-audit-system.herokuapp.com')),
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1:8000')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1:8000,inventory-audit-system.herokuapp.com')),
 
     /*
     |--------------------------------------------------------------------------
